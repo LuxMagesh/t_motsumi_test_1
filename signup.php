9e31d9e29d5589238ac28f9b1a7900eba28016fb
Tshegofatso Motsumi <luxmotsumi@gmail.com>
2022-07-09 15:42:03 +0200

    Display Error a Few and Refesh with a Redirect

--- signup.php
+++ signup.php
@@ -4,6 +4,7 @@ $message_id_already_exists = '';
 $message_successfully_inserted_into_db = '';
 $message_idnumber_thirteen_digits = '';
 $message_idnumber_numeric = '';
+$message_not_birthday = "";
 if ( isset( $_POST['submit'] ) )
 {
 
@@ -14,14 +15,18 @@ if ( isset( $_POST['submit'] ) )
     $date = $_POST['reg_date'];
     $new_date = date("d/m/Y", strtotime($date));
 
-    // echo "<pre>";
-    // echo gettype($new_date2);
-    //     print_r($new_date2);
-    // echo "</pre>";
-
     if (is_numeric($idnumber) && strlen($idnumber) === 13) {
 
+      /***********************************************************
+			       Getting the Date of Birth from the ID Number
+		  ***********************************************************/
+
       $dateboss = substr($idnumber, 0, 6);
+
+      /***********************************************************
+			  Splitting the string to Get the Day , Month and the Year
+		  ***********************************************************/ 
+
       $num_array = str_split($dateboss);
 
       $id_year = $num_array[0] . $num_array[1];
@@ -30,27 +35,38 @@ if ( isset( $_POST['submit'] ) )
   
       if ( $id_year < 1 || $id_year > 2022) {
         $errors = true;
+        header( "refresh:5;url=/reg.php" );
+        exit();
       }else {
         echo "Year: " . $id_year . "<br>";
       } 
       if ( $id_month < 1 || $id_month > 12) {
         $errors = true;
+        header( "refresh:5;url=/reg.php" );
+        exit();
       }else {
         echo "Month: " . $id_month . "<br>";
       } 
   
       if ( $id_day < 1 || $id_day > 31) {
         $errors = true;
+        header( "refresh:5;url=/reg.php" );
+        exit();
       }else {
         echo "Day: " . $id_day . "<br>";
       } 
-  
+
+      /***********************************************************
+			    Adding the correct Date of Birth in to the Database
+		  ***********************************************************/
+
      $finalYear = "". $id_day."/". $id_month."/". $id_year."";
+
 
      if(!$finalYear === $new_date)
      {
-      $message_id_already_exists = "<center><h1 class='white-text card'>Sorry...ID NUmber Provided , Does Not Match the Date of Birth you entered on the system !!!</h1></center>";
-      print_r($message_id_already_exists);
+      $message_not_birthday = "<center><h1 class='white-text card'>Sorry...ID NUmber Provided , Does Not Match the Date of Birth you entered on the system !!!</h1></center>";
+      print_r($message_not_birthday);
      }else{
       $errors = false;
       require 'dbcon.php';
@@ -62,6 +78,11 @@ if ( isset( $_POST['submit'] ) )
           print_r($message_id_already_exists);
       }
           else{
+
+            /**********************************************************************
+                Inserting the Correct Date to the Database, After Various Checks
+            ***********************************************************************/ 
+            
               $sql="INSERT INTO `users`(`f_name`,`l_name`,`id_num`,`reg_date`) 
               VALUES ('$fname','$lname','$idnumber','$finalYear')";
               
@@ -73,10 +94,11 @@ if ( isset( $_POST['submit'] ) )
               } 
       }
      }
-    
-       
      }
      else {
+            /**********************************************************************
+                          Failed to insert due to non valid reasons
+            ***********************************************************************/ 
         $message_idnumber_thirteen_digits = "<center><h1 class='white-text card'>Sign Up Failed Either Because ==> 1 = Contains Non Numerical Characters || 2 = Not a Valid ID Number !!! Enter a Valid Record to Succeed </h1></center>";
         print_r($message_idnumber_thirteen_digits);
       }
